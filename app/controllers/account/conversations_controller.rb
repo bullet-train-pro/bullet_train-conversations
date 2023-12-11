d09cb2cc0ac37e9a087c5f0a4355bf2c99a6186f
Seth Siegler <usaseth@gmail.com>
2023-12-11 13:57:34 -0800

    checking if these are defined before calling them

--- app/controllers/account/conversations_controller.rb
+++ app/controllers/account/conversations_controller.rb
@@ -1,7 +1,7 @@
 class Account::ConversationsController < Account::ApplicationController
   include Conversations::BaseController
 
-  # account_load_and_authorize_resource :conversation, through: BulletTrain::Conversations.parent_association, through_association: :conversations, except: [:create]
+  account_load_and_authorize_resource :conversation, through: BulletTrain::Conversations.parent_association, through_association: :conversations, except: [:create] if defined?(account_load_and_authorize_resource)
 
   def author
     :membership
